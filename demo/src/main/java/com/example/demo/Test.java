cdeae19842332b52dd6315c3d2109dc94b065990
wdnsszm <15610416128@163.com>
2018-12-09 11:12:08 +0800

    “测试"

---
+++ demo/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangmaozhuang
+ *
+ * 2018年9月18日 下午9:11:28
+ */
+@RestController
+@RequestMapping("/11")
+public class Test {
+	@RequestMapping("/test")
+	public String fun() {
+		return "success";
+	}
+
+}
